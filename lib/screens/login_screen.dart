bc61f194c73a7b5b13bfad33ffc8f063c3434e9b
SUMAN_SAURAV <me.suman2113@gmail.com>
2024-05-16 14:48:56 +0000

    login page

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool passToggle = true;
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Image.asset("images/Lawyer-pana.png"),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.all(12),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text("Enter Username"),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: EdgeInsets.all(12),
+                child: TextField(
+                  obscureText: passToggle ? true : false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    label: Text("Enter Password"),
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        if (passToggle == true) {
+                          passToggle = false;
+                        } else {
+                          passToggle = true;
+                        }
+                        setState(() {});
+                      },
+                      child: passToggle
+                          ? Icon(
+                              CupertinoIcons.eye_slash_fill,
+                            )
+                          : Icon(
+                              CupertinoIcons.eye_fill,
+                            ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                child: Material(
+                  color: Color(0xFF7165D6),
+                  borderRadius: BorderRadius.circular(10),
+                  child: InkWell(
+                    onTap: () {
+                      // Navigator.push(context, MaterialPageRoute(
+                      // builder:(context)=>
+                      // ),);
+                    },
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
